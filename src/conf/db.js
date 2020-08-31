484e6a25a43f335ee1a60120ef4dc0384b8574ec
zzyo <1977795940@qq.com>
2020-08-31 17:56:22 +0800

    接入redis

--- src/conf/db.js
+++ src/conf/db.js
@@ -2,23 +2,23 @@ const env = process.env.NODE_ENV  // 环境参数
 
 // 配置
 let MYSQL_CONF
-// let REDIS_CONF
+let REDIS_CONF
 
 if (env === 'dev') {
   // mysql
   MYSQL_CONF = {
     host: 'localhost',
     user: 'root',
-    password: '123456',
+    password: 'Mysql_2018',
     port: '3306',
     database: 'myblog'
   }
 
   // redis
-  // REDIS_CONF = {
-  //   port: 6379,
-  //   host: '127.0.0.1'
-  // }
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+  }
 }
 
 if (env === 'production') {
@@ -32,13 +32,13 @@ if (env === 'production') {
   }
 
   // redis
-  // REDIS_CONF = {
-  //   port: 6379,
-  //   host: '127.0.0.1'
-  // }
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+  }
 }
 
 module.exports = {
   MYSQL_CONF,
-  // REDIS_CONF
+  REDIS_CONF
 }
